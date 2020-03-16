9f6c81ac64be7f0388b47799291b13f4e5730958
Mark Whang <hwhang0917@gmail.com>
2020-03-15 18:22:46 -0700

    Add private/public middleware

--- src/middleware.js
+++ src/middleware.js
@@ -6,3 +6,21 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.routes = routes;
   next();
 };
+
+// Middleware to prevent non-logged-in users from accessing certain page
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.landingPage);
+  } else {
+    next();
+  }
+};
+
+// Middleware to prevent logged-in users from accessing certain page
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.dashboard);
+  }
+};
